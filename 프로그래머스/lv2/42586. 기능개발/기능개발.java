236ddeffa1788fdba78217a9fe270ee2f4afdcd6
JIHEE CHOI <110098108+CJIHEE@users.noreply.github.com>
2023-07-04 16:29:37 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.java
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -2,17 +2,16 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
+        
         int[] newProgresses = new int[progresses.length]; 
         List<Integer> progressesList = new ArrayList<>();
         
+        //남은 작업일 수 구하기
         for(int i=0; i<progresses.length; i++){
             newProgresses[i] = (100-progresses[i])/speeds[i];
-            if ((100 - progresses[i]) % speeds[i] != 0){
-                newProgresses[i]  += 1;
-            }                     
         }
         
+        //내보낼 지 결정
         int count = 1;
         int x = newProgresses[0];
         for(int i=1; i<newProgresses.length; i++){
@@ -25,15 +24,14 @@ class Solution {
                 x = newProgresses[i];
             }
         }
-        progressesList.add(count);
         
         answer = new int[progressesList.size()];
         for(int i=0; i<progressesList.size(); i++){
             answer[i] = progressesList.get(i);
         }    
-    
+        
         return answer; 
-    
+        
     }       
         
 }
