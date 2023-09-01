3fdd093400b20b930c86f9fe65e43a0f98a91f1c
JIHEE CHOI <110098108+CJIHEE@users.noreply.github.com>
2023-09-01 14:43:40 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 204 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n+1];
+
+        for(int i=2; i<=n; i++){
+           arr[i] = i;
+        }
+
+        System.out.print(solution(n,k,arr));
+
+    }
+
+    private static int solution(int n, int k, int arr[]) {
+        int count =0;
+        int answer = 0;
+
+        for(int i=2; i<=n; i++){
+            if(arr[i] !=-1){
+                arr[i] = -1;
+                count++;
+                if(count == k){
+                    return i;
+                }
+                for(int j=i+1; j<=n; j++){
+                    if(arr[j]%i == 0){
+                        arr[j] =-1;
+                        count++;
+                        if(count == k){
+                            return j;
+                        }
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
