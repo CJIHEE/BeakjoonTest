3fd007785ec40ebe5244cd932ff6fdf5c088e655
JIHEE CHOI <110098108+CJIHEE@users.noreply.github.com>
2024-09-17 19:11:10 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -1,12 +1,10 @@
 class Solution {
     public int solution(int slice, int n) {
-        int answer = 1;
-        if(n > slice){
-            for(int i=1; i<= n; i++){
-                if(slice * i >= n){
-                    return answer = i;
-                }
-            }
+        int answer = 0;
+        if(n%slice == 0){
+            answer = n/slice;
+        }else{
+            answer = n/slice+1;
         }
         return answer;
     }
