cd546861a7e53bf06ea1c4c2e0843a646d315e9f
JIHEE CHOI <110098108+CJIHEE@users.noreply.github.com>
2024-10-22 20:40:34 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 29.02 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
 
 ### 성능 요약
 
-메모리: 112 MB, 시간: 31.36 ms
+메모리: 112 MB, 시간: 29.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 22일 18:06:32
+2024년 10월 22일 20:40:31
 
 ### 문제 설명
 
--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,28 +1,23 @@
 import java.util.*;
 
 public class Solution {
-    public int[] solution(int [] arr) {
+    public int[] solution(int []arr) {
         
         Queue<Integer> queue = new LinkedList<>();
-        int lastNum = -1;
-        for(int i=0; i<arr.length; i++){
-            if(queue.isEmpty()){
-                queue.offer(arr[i]);
-                lastNum = arr[i];
-            }else{
-                if(lastNum != arr[i]){
-                    queue.offer(arr[i]);
-                    lastNum = arr[i];
-                }
+        int target = -1;
+        for(int x : arr){
+            if(x != target){
+                queue.offer(x);
+                target = x;
             }
         }
+        
         int size = queue.size();
+        
         int[] answer = new int[size];
         
-        for(int i =0; i < size; i++){
-            if(!queue.isEmpty()){
-                answer[i] = queue.poll();
-            }
+        for(int i=0; i < size; i++){
+            answer[i] = queue.poll();
         }
 
         return answer;
