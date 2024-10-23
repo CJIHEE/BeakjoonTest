bc73229524b24f051ade1be9c837c3582b6148a7
JIHEE CHOI <110098108+CJIHEE@users.noreply.github.com>
2024-10-24 04:45:06 +0900

    [Silver IV] Title: 수 찾기, Time: 1128 ms, Memory: 61668 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,31 +1,29 @@
+import java.io.*;
 import java.util.*;
+
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        StringTokenizer st = new StringTokenizer(bf.readLine());
 
-        Integer[] arrN = new Integer[n];
-        for(int i=0; i< n; i++) arrN[i] = sc.nextInt();
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        Arrays.sort(arrN);
-
-        int m = sc.nextInt();
-
-        int[] arrM = new int[m];
-        for(int i=0; i<m; i++) arrM[i] =sc.nextInt();
-
-        solution(n,arrN,m,arrM);
-    }
-    public static void solution(int n ,Integer[] arrN, int m, int[]arrM){
-        Set<Integer> setN = new HashSet<Integer>(Arrays.asList(arrN));
-        for(int i=0; i<m; i++){
-            if(setN.contains(arrM[i])){
-                System.out.println(1);
-            }
-            else{
-                System.out.println(0);
-            }
+        for(int i=0; i < n; i++){
+            map.put(Integer.parseInt(st.nextToken()), i);
         }
+
+        int m = Integer.parseInt(bf.readLine());
+        st = new StringTokenizer(bf.readLine());
+        for( int i=0; i < m; i++){
+            System.out.println(solution(Integer.parseInt(st.nextToken()), map));
+        }
+
+    }
+    private static int solution(int m, HashMap map){
+        int answer = 0;
+        if(map.containsKey(m)) answer =1;
+
+        return answer;
     }
 }
-
