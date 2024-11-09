2b99dee5da5c70380cd168fcf9ebe34b5c01e827
JIHEE CHOI <110098108+CJIHEE@users.noreply.github.com>
2024-11-10 00:42:31 +0900

    [level 2] Title: 올바른 괄호, Time: 19.61 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 67.5 MB, 시간: 30.28 ms
+메모리: 53.8 MB, 시간: 19.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 17일 18:36:52
+2024년 11월 10일 00:42:29
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,37 +1,22 @@
 import java.util.*;
-
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
 
-        Queue queue = new LinkedList<>();
-        for(char x : s.toCharArray() ){
-            queue.add(Character.toString(x));
-        }
-        int flag = 0;
-        int count =0;
-        int compareCount =0;
-       
-        while(!queue.isEmpty()){
-            if(")".equals(queue.peek()) && flag==0 ){
-                return false;
-            }
+        Stack stack = new Stack<>();
+        
+        for(char x : s.toCharArray()){
+            if(x == '(') stack.push('(');
             else {
-                if("(".equals(queue.peek()) ){
-                    count++;
-                }else{
-                    compareCount++;
-                }
-                flag++;
-                queue.poll();   
-            }
-            if(count == compareCount){
-                flag = 0;
+                if(stack.isEmpty()) return answer = false;
+                else stack.pop();
             }
         }
-        if(count != compareCount){
-            answer = false;
+        
+        if(!stack.isEmpty()){
+          answer =false;  
         }
+
         return answer;
     }
 }
