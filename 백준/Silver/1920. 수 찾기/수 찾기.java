1f79d51e3132873379ab94038815db9109b9b2c9
JIHEE CHOI <110098108+CJIHEE@users.noreply.github.com>
2024-11-10 01:00:48 +0900

    [Silver IV] Title: 수 찾기, Time: 1096 ms, Memory: 58584 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,29 +1,30 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
+    static HashSet<Integer> set = new HashSet<>();
+
     public static void main(String[] args) throws IOException{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bf.readLine());
         StringTokenizer st = new StringTokenizer(bf.readLine());
 
-        HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int i=0; i < n; i++){
-            map.put(Integer.parseInt(st.nextToken()), i);
+        for(int i =0; i<n; i++){
+            set.add(Integer.parseInt(st.nextToken()));
         }
 
         int m = Integer.parseInt(bf.readLine());
         st = new StringTokenizer(bf.readLine());
-        for( int i=0; i < m; i++){
-            System.out.println(solution(Integer.parseInt(st.nextToken()), map));
+        for(int i=0; i<m; i++){
+            System.out.println(solution(Integer.parseInt(st.nextToken())));
         }
-
     }
-    private static int solution(int m, HashMap map){
-        int answer = 0;
-        if(map.containsKey(m)) answer =1;
+
+    private static int solution(int num){
+        int answer =0 ;
+        if(set.contains(num)) answer = 1;
 
         return answer;
     }
+
 }
